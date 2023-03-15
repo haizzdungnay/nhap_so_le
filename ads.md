92870bda8cd045873234e8238d46b61abe0c37be
haizzdungnay <126995899+haizzdungnay@users.noreply.github.com>
2023-03-15 16:22:12 +0700

    Add files via upload

---
+++ ads.md
@@ -0,0 +1,3 @@
+Hãy sử dụng vòng lặp for để in ra các số lẻ từ 10 đến 20
+##### output:
+> 11 13 15 17 19 
---
+++ solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std ;
+
+int main ()
+{
+for(int i = 11 ; i <= 19 ; i++)
+{
+    cout << i << " " ;
+}
+return 0 ;
+}
